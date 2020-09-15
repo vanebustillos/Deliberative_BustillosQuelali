f932ce71e7f49e529be2b5364df24c07aa77e3a5
vane_bustillos <vanebustillos1@gmail.com>
2020-09-15 12:51:02 -0400

    Refactoring and cleaning code.

--- src/main/java/deliberative/bustillos_Quelali/AuxiliarOperations.java
+++ src/main/java/deliberative/bustillos_Quelali/AuxiliarOperations.java
@@ -15,10 +15,10 @@ public class AuxiliarOperations {
 
     public List<State> getSuccessors(State state) {
         List<State> successors = new ArrayList<>();
-        for (Task taskPickup: state.packagesToPickup) {
-            if(isPossiblePickup(state, taskPickup)) {
+        for (Task taskPickup : state.packagesToPickup) {
+            if (isPossiblePickup(state, taskPickup)) {
                 State successor = clone(state);
-                if(!taskPickup.pickupCity.equals(state.currentCity)){
+                if (!taskPickup.pickupCity.equals(state.currentCity)) {
                     List<City> path = successor.getCurrentCity().pathTo(taskPickup.pickupCity);
                     processMoves(successor, path);
                     successor.currentCity = taskPickup.pickupCity;
@@ -27,9 +27,9 @@ public class AuxiliarOperations {
                 successors.add(successor);
             }
         }
-        for (Task taskDelivery: state.packagesToDelivery) {
+        for (Task taskDelivery : state.packagesToDelivery) {
             State successor = clone(state);
-            if(!taskDelivery.deliveryCity.equals(state.currentCity)){
+            if (!taskDelivery.deliveryCity.equals(state.currentCity)) {
                 List<City> path = successor.getCurrentCity().pathTo(taskDelivery.deliveryCity);
                 processMoves(successor, path);
                 successor.currentCity = taskDelivery.deliveryCity;
@@ -40,59 +40,59 @@ public class AuxiliarOperations {
         return successors;
     }
 
-    public Double realCost(Vehicle vehicle,City city1, City city2) {
-        List<City> path = city1.pathTo(city2);
-        double distance = 0.0;
-        City last = city1;
-        for (City city:path) {
-            distance = distance + last.distanceTo(city);
-            last = city;
-        }
-        //return city1.distanceTo(city2) * vehicle.costPerKm();
-        return distance * vehicle.costPerKm();
+    public Double realCost(Vehicle vehicle, City city1, City city2) {
+        return city1.distanceTo(city2) * vehicle.costPerKm();
     }
 
-    public Double h1(Vehicle vehicle,State state) {
-        Double maxDistance = 0.0;
+    /*
+     * Heuristic that provides the cost that the agent will spend if it goes to the farthest city
+     * */
+    public double h1(Vehicle vehicle, State state) {
+        double maxDistance = 0.0;
         City farthestCity = null;
-        if(!state.getPackagesToPickup().isEmpty()){
-            for (Task task: state.getPackagesToPickup()) {
-                Double distance = state.getCurrentCity().distanceTo(task.pickupCity);
-                if (distance > maxDistance) {
-                    maxDistance = distance;
-                    farthestCity = task.pickupCity;
-                }
+
+        for (Task task : state.getPackagesToPickup()) {
+            Double distance = state.getCurrentCity().distanceTo(task.pickupCity);
+            if (distance > maxDistance) {
+                maxDistance = distance;
+                farthestCity = task.pickupCity;
             }
         }
-        else {
-            for (Task task : state.getPackagesToDelivery()) {
-                Double distance = state.getCurrentCity().distanceTo(task.deliveryCity);
-                if (distance > maxDistance) {
-                    maxDistance = distance;
-                    farthestCity = task.deliveryCity;
-                }
+        for (Task task : state.getPackagesToDelivery()) {
+            double distance = state.getCurrentCity().distanceTo(task.deliveryCity);
+            if (distance > maxDistance) {
+                maxDistance = distance;
+                farthestCity = task.deliveryCity;
             }
         }
 
         return realCost(vehicle, state.getCurrentCity(), farthestCity);
     }
 
-
-    public Double h2(Vehicle vehicle, State state) {
-        Double cost = 0.0;
-        for (Task task: state.getPackagesToPickup()) {
-            cost = cost + realCost(vehicle, state.getCurrentCity(), task.pickupCity);
+    /*
+     * Heuristic that calculates the sumatory of the cost that the agent has to spend
+     *  by moving between cities for pickup or deliver a package.
+     *
+     * When the agent deliver a package, it obtains a reward for it.
+     * */
+    public double h2(Vehicle vehicle, State state) {
+        double cost = 0.0;
+        for (Task task : state.getPackagesToPickup()) {
+            //cost = cost + realCost(vehicle, state.getCurrentCity(), task.pickupCity);
+            cost = cost + realCost(vehicle, state.getCurrentCity(), task.pickupCity) - task.reward;
         }
-        for (Task task: state.getPackagesToDelivery()) {
-            Double netCost = realCost(vehicle, state.getCurrentCity(), task.deliveryCity) - task.reward;
+       /* for (Task task : state.getPackagesToDelivery()) {
+            //double netCost = task.reward - realCost(vehicle, state.getCurrentCity(), task.deliveryCity);
+            double netCost = realCost(vehicle, state.getCurrentCity(), task.deliveryCity) - task.reward;
             cost = cost + netCost;
-        }
+        }*/
         return cost;
 
     }
-    public double h3(State state){
-        if(state.getPackagesToPickup().size() != 0 && state.getPackagesToDelivery().size() != 0){
-        return state.getPackagesToDelivery().size() / state.getPackagesToPickup().size();
+
+    public double h3(State state) {
+        if (state.getPackagesToPickup().size() != 0 && state.getPackagesToDelivery().size() != 0) {
+            return state.getPackagesToDelivery().size() / state.getPackagesToPickup().size();
         }
         return 0;
     }
@@ -113,15 +113,25 @@ public class AuxiliarOperations {
             successor.actions.add(new Move(city));
         }
     }
-    private double availableCapacity(State state){
+
+    /*
+     * Getting the available capacity of the vehicle
+     * */
+    private double availableCapacity(State state) {
         double currentCapacity = state.packagesToPickup.weightSum();
         return state.getCapacity() - currentCapacity;
     }
 
-    private boolean isPossiblePickup(State state, Task taskPickup){
+    /*
+     * The agent can pickup a package if it has available capacity in the vehicle
+     * */
+    private boolean isPossiblePickup(State state, Task taskPickup) {
         return (availableCapacity(state) > 0 && taskPickup.weight <= availableCapacity(state));
     }
 
+    /*
+     * A terminal state is the state that doesn't have any package to pickup or deliver.
+     * */
     public boolean isGoalState(State state) {
         return state.packagesToDelivery.isEmpty() && state.packagesToPickup.isEmpty();
     }
--- src/main/java/deliberative/bustillos_Quelali/DeliberativeTemplate_BustillosQuelali.java
+++ src/main/java/deliberative/bustillos_Quelali/DeliberativeTemplate_BustillosQuelali.java
@@ -1,16 +1,15 @@
 package deliberative.bustillos_Quelali;
 
 /* import table */
+
 import logist.agent.Agent;
 import logist.behavior.DeliberativeBehavior;
 import logist.plan.Action;
 import logist.plan.Plan;
 import logist.simulation.Vehicle;
-import logist.task.Task;
 import logist.task.TaskDistribution;
 import logist.task.TaskSet;
 import logist.topology.Topology;
-import logist.topology.Topology.City;
 
 import java.util.*;
 
@@ -20,90 +19,84 @@ import java.util.*;
 @SuppressWarnings("unused")
 public class DeliberativeTemplate_BustillosQuelali implements DeliberativeBehavior {
 
-	enum Algorithm { BFS, ASTAR }
+    enum Algorithm {BFS, ASTAR}
 
-	/* Environment */
-	Topology topology;
-	TaskDistribution td;
-	
-	/* the properties of the agent */
-	Agent agent;
-	int capacity;
+    /* Environment */
+    Topology topology;
+    TaskDistribution td;
 
-	/* the planning class */
-	Algorithm algorithm;
+    /* the properties of the agent */
+    Agent agent;
+    int capacity;
 
-	AuxiliarOperations operations = new AuxiliarOperations();
-	
-	@Override
-	public void setup(Topology topology, TaskDistribution td, Agent agent) {
-		this.topology = topology;
-		this.td = td;
-		this.agent = agent;
-		// initialize the planner
-		capacity = agent.vehicles().get(0).capacity();
-		String algorithmName = agent.readProperty("algorithm", String.class, "ASTAR");
-		
-		// Throws IllegalArgumentException if algorithm is unknown
-		algorithm = Algorithm.valueOf(algorithmName.toUpperCase());
+    /* the planning class */
+    Algorithm algorithm;
 
-	}
-	
-	@Override
-	public Plan plan(Vehicle vehicle, TaskSet tasks) {
-		Plan plan;
-		// Compute the plan with the selected algorithm.
-		switch (algorithm) {
-		case ASTAR:
-			for (Task task: tasks) {
-				System.out.println(task);
-			}
-			plan = aStar(vehicle,tasks);
-			break;
-		case BFS:
-			plan = bfs(vehicle,tasks);
-			break;
-		default:
-			throw new AssertionError("Should not happen.");
-		}		
-		return plan;
-	}
+    AuxiliarOperations operations = new AuxiliarOperations();
 
-	private Plan bfs(Vehicle vehicle, TaskSet tasks) {
-		Plan plan = new Plan(vehicle.getCurrentCity());
-		State initialState = new State(vehicle.getCurrentCity(), tasks, vehicle.getCurrentTasks(), Collections.emptyList(), capacity);
-		LinkedList<State> q = new LinkedList<>();
-		q.add(initialState);
-		do {
-			State state = q.removeFirst();
-			if (operations.isGoalState(state)) {
-				for (Action action: state.getActions()) {
-					plan.append(action);
-				}
-				break;
-			}
-			q.addAll(operations.getSuccessors(state));
-		} while (!q.isEmpty());
-		return plan;
-	}
+    @Override
+    public void setup(Topology topology, TaskDistribution td, Agent agent) {
+        this.topology = topology;
+        this.td = td;
+        this.agent = agent;
+        // initialize the planner
+        capacity = agent.vehicles().get(0).capacity();
+        String algorithmName = agent.readProperty("algorithm", String.class, "ASTAR");
 
-	private Plan aStar(Vehicle vehicle, TaskSet tasks) {
-		Plan plan = new Plan(vehicle.getCurrentCity());
-		State initialState = new State(vehicle.getCurrentCity(), tasks, vehicle.getCurrentTasks(), Collections.emptyList(),capacity);
-		List<State> border = new ArrayList<>();
-		border.add(initialState);
-		while (!border.isEmpty()) {
-			State bestState = null;
-			//-------------------H3
-			/*double minHeuristic = Double.MAX_VALUE;
-			for (State potentialNext: border) {
-				if (operations.h3(potentialNext) < minHeuristic) {
-					minHeuristic = operations.h3( potentialNext);
-					bestState = potentialNext;
-				}
-			}*/
-			//-------------------H2
-			Double maxHeuristic = - Double.MAX_VALUE;
+        // Throws IllegalArgumentException if algorithm is unknown
+        algorithm = Algorithm.valueOf(algorithmName.toUpperCase());
+
+    }
+
+    @Override
+    public Plan plan(Vehicle vehicle, TaskSet tasks) {
+        Plan plan;
+        switch (algorithm) {
+            case ASTAR:
+                plan = aStar(vehicle, tasks);
+                break;
+            case BFS:
+                plan = bfs(vehicle, tasks);
+                break;
+            default:
+                throw new AssertionError("Should not happen.");
+        }
+        return plan;
+    }
+
+    private Plan bfs(Vehicle vehicle, TaskSet tasks) {
+        Plan plan = new Plan(vehicle.getCurrentCity());
+        State initialState = new State(vehicle.getCurrentCity(), tasks, vehicle.getCurrentTasks(), Collections.emptyList(), capacity);
+        LinkedList<State> q = new LinkedList<>();
+        q.add(initialState);
+        do {
+            State state = q.removeFirst();
+            if (operations.isGoalState(state)) {
+                return appendActions(plan, state);
+            }
+            q.addAll(operations.getSuccessors(state));
+        } while (!q.isEmpty());
+        return plan;
+    }
+
+    private Plan aStar(Vehicle vehicle, TaskSet tasks) {
+        Plan plan = new Plan(vehicle.getCurrentCity());
+        State initialState = new State(vehicle.getCurrentCity(), tasks, vehicle.getCurrentTasks(), Collections.emptyList(), capacity);
+        List<State> border = new ArrayList<>();
+        border.add(initialState);
+        while (!border.isEmpty()) {
+            State bestState = null;
+
+            //-------------------H3
+            /*double minHeuristic = Double.MAX_VALUE;
+            for (State potentialNext : border) {
+                if (operations.h3(potentialNext) < minHeuristic) {
+                    minHeuristic = operations.h3(potentialNext);
+                    bestState = potentialNext;
+                }
+            }*/
+            //-------------------H2 max
+			double maxHeuristic = - Double.MAX_VALUE;
 			for (State potentialNext: border) {
 				if (operations.h2(vehicle,potentialNext) > maxHeuristic) {
 					maxHeuristic = operations.h2(vehicle, potentialNext);
@@ -111,29 +104,42 @@ public class DeliberativeTemplate_BustillosQuelali implements DeliberativeBehavi
 				}
 			}
 			//-------------------
-			if(bestState == null){
-				throw new IllegalStateException("Unexpected state left.");
-			}
-			border.remove(bestState);
-			if (operations.isGoalState(bestState)) {
-				for (Action action: bestState.getActions()) {
-					plan.append(action);
+            //-------------------H2 min
+			/*double minHeuristic = Double.MAX_VALUE;
+			for (State potentialNext: border) {
+				if (operations.h2(vehicle,potentialNext) < minHeuristic) {
+					minHeuristic = operations.h2(vehicle, potentialNext);
+					bestState = potentialNext;
 				}
-				return plan;
-			}
-			List<State> successors = operations.getSuccessors(bestState);
-			border.addAll(successors);
-		}
-		return null;
-	}
+			}*/
+            //-------------------------
+            if (bestState == null) {
+                throw new IllegalStateException("Unexpected state left.");
+            }
+            border.remove(bestState);
+            if (operations.isGoalState(bestState)) {
+                return appendActions(plan, bestState);
+            }
+            List<State> successors = operations.getSuccessors(bestState);
+            border.addAll(successors);
+        }
+        return plan;
+    }
 
-	@Override
-	public void planCancelled(TaskSet carriedTasks) {
-		
-		if (!carriedTasks.isEmpty()) {
-			// This cannot happen for this simple agent, but typically
-			// you will need to consider the carriedTasks when the next
-			// plan is computed.
-		}
-	}
+    private Plan appendActions(Plan plan, State bestState) {
+        for (Action action : bestState.getActions()) {
+            plan.append(action);
+        }
+        return plan;
+    }
+
+    @Override
+    public void planCancelled(TaskSet carriedTasks) {
+
+        if (!carriedTasks.isEmpty()) {
+            // This cannot happen for this simple agent, but typically
+            // you will need to consider the carriedTasks when the next
+            // plan is computed.
+        }
+    }
 }
